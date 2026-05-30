b8b577632b74dfa426545c4b0199c7e42461069a
arthur <103393571+THUR165@users.noreply.github.com>
2026-05-30 13:51:10 -0300

    feat: configura painel administrativo dos modelos principais

--- examexpj/backend/core/admin.py
+++ examexpj/backend/core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Usuario, Disciplina, Topico, Questao, Alternativa
 
-# Register your models here.
+admin.site.register(Usuario, UserAdmin)
+admin.site.register(Disciplina)
+admin.site.register(Topico)
+class AlternativaInline(admin.TabularInline):
+    model = Alternativa
+    extra = 4
+
+class QuestaoAdmin(admin.ModelAdmin):
+    list_display = ('enunciado', 'topico', 'tipo', 'peso')
+    inlines = [AlternativaInline]
+
+admin.site.register(Questao, QuestaoAdmin)
